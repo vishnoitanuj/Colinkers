d4c90a5129bc3ed7b7ca031ac6719c865ed91c5d
Tanuj <vishnoitanuj@outlook.com>
2017-07-04 13:13:33 +0530

    Handing to Yuvraj

--- sendemail.php
+++ sendemail.php
@@ -11,11 +11,12 @@
     $message = @trim(stripslashes($_POST['message'])); 
 
     $email_from = $email;
-    $email_to = 'email@email.com';//replace with your email
+    $email_to = 'contact@colinkers.in';//replace with your email
 
     $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
     $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
 
     echo json_encode($status);
-    die;
+    die
+?>
